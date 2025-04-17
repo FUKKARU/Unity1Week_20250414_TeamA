64a5dff4a5a9460e092b233f956bcd2782985e46
foriver4725 <hrt67@icloud.com>
2025-04-17 23:01:03 +0900

    upgrader result directions (added dotweem)

--- Assets/_MyAssets/Scripts/General/SceneManager.cs
+++ Assets/_MyAssets/Scripts/General/SceneManager.cs
@@ -8,6 +8,15 @@ namespace NGeneral
         Title,
         StageSelect,
         Stage1,
+        Stage2,
+        Stage3,
+        Stage4,
+        Stage5,
+        Stage6,
+        Stage7,
+        Stage8,
+        Stage9,
+        Stage10,
     }
 
     public static class SceneManager
@@ -28,12 +37,61 @@ namespace NGeneral
             UniSceneManager.LoadScene(sceneName);
         }
 
+        /// <summary>
+        /// 次シーンを取得 (最終ステージの場合は、タイトル)
+        /// </summary>
+        public static Scene NextScene() => UniSceneManager.GetActiveScene().name.ToSceneEnum() switch
+        {
+            Scene.Title => Scene.StageSelect,
+            Scene.StageSelect => Scene.Stage1,
+            Scene.Stage1 => Scene.Stage2,
+            Scene.Stage2 => Scene.Stage3,
+            Scene.Stage3 => Scene.Stage4,
+            Scene.Stage4 => Scene.Stage5,
+            Scene.Stage5 => Scene.Stage6,
+            Scene.Stage6 => Scene.Stage7,
+            Scene.Stage7 => Scene.Stage8,
+            Scene.Stage8 => Scene.Stage9,
+            Scene.Stage9 => Scene.Stage10,
+            Scene.Stage10 => Scene.Title,
+            _ => Scene.Title,
+        };
+
         private static string ToSceneName(this Scene scene) => scene switch
         {
             Scene.Title => "Title",
             Scene.StageSelect => "StageSelect",
             Scene.Stage1 => "Stage1",
+            Scene.Stage2 => "Stage2",
+            Scene.Stage3 => "Stage3",
+            Scene.Stage4 => "Stage4",
+            Scene.Stage5 => "Stage5",
+            Scene.Stage6 => "Stage6",
+            Scene.Stage7 => "Stage7",
+            Scene.Stage8 => "Stage8",
+            Scene.Stage9 => "Stage9",
+            Scene.Stage10 => "Stage10",
             _ => string.Empty,
         };
+
+        private static Scene ToSceneEnum(this string sceneName)
+        {
+            return sceneName switch
+            {
+                "Title" => Scene.Title,
+                "StageSelect" => Scene.StageSelect,
+                "Stage1" => Scene.Stage1,
+                "Stage2" => Scene.Stage2,
+                "Stage3" => Scene.Stage3,
+                "Stage4" => Scene.Stage4,
+                "Stage5" => Scene.Stage5,
+                "Stage6" => Scene.Stage6,
+                "Stage7" => Scene.Stage7,
+                "Stage8" => Scene.Stage8,
+                "Stage9" => Scene.Stage9,
+                "Stage10" => Scene.Stage10,
+                _ => Scene.Title,
+            };
+        }
     }
 }
--- Assets/_MyAssets/Scripts/InGame/ResultManager.cs
+++ Assets/_MyAssets/Scripts/InGame/ResultManager.cs
@@ -1,7 +1,8 @@
-using NGeneral;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 using Text = TMPro.TextMeshProUGUI;
+using NGeneral;
 
 namespace NInGame
 {
@@ -11,6 +12,9 @@ namespace NInGame
         [SerializeField] private Text text;
         [SerializeField] private Button retryButton;
         [SerializeField] private Button backButton;
+        [SerializeField] private Text retryButtonText;
+        [SerializeField] private Text backButtonText;
+        [SerializeField] private Image frontBlockingImage;
 
         private bool isShowing = false;
         private bool hasButtonClicked = false;
@@ -18,13 +22,10 @@ namespace NInGame
         private void Start()
         {
             if (parent != null)
+            {
                 parent.gameObject.SetActive(false);
-
-            if (retryButton != null)
-                retryButton.onClick.AddListener(() => OnLoadSceneButtonClicked(null));
-
-            if (backButton != null)
-                backButton.onClick.AddListener(() => OnLoadSceneButtonClicked(Scene.StageSelect));
+                parent.anchoredPosition = new Vector2(0, 10);
+            }
         }
 
         public void Show(bool cleared)
@@ -32,10 +33,48 @@ namespace NInGame
             if (isShowing) return;
             isShowing = true;
 
-            Time.timeScale = 0f;
+            if (cleared)
+            {
+                if (text != null)
+                    text.text = "I CLEARED the stage!!!";
+                if (retryButton != null)
+                    retryButtonText.text = "I go to NEXT";
+                if (backButton != null)
+                    backButtonText.text = "I'll be BACK";
 
-            if (text != null) text.text = cleared ? "Game Clear !!!" : "Failed ...";
-            if (parent != null) parent.gameObject.SetActive(true);
+                if (retryButton != null)
+                    retryButton.onClick.AddListener(() => OnLoadSceneButtonClicked(SceneManager.NextScene()));
+                if (backButton != null)
+                    backButton.onClick.AddListener(() => OnLoadSceneButtonClicked(Scene.StageSelect));
+            }
+            else
+            {
+                if (text != null)
+                    text.text = "I FAILED...";
+                if (retryButton != null)
+                    retryButtonText.text = "I wanna RETRY";
+                if (backButton != null)
+                    backButtonText.text = "I'll be BACK";
+
+                if (retryButton != null)
+                    retryButton.onClick.AddListener(() => OnLoadSceneButtonClicked(null));
+                if (backButton != null)
+                    backButton.onClick.AddListener(() => OnLoadSceneButtonClicked(Scene.StageSelect));
+            }
+
+            if (parent != null)
+            {
+                parent.gameObject.SetActive(true);
+                parent.DOAnchorPosY(0, 0.5f)
+                    .SetEase(Ease.OutBack)
+                    .OnComplete(() =>
+                    {
+                        if (frontBlockingImage != null)
+                            frontBlockingImage.gameObject.SetActive(false);
+
+                        Time.timeScale = 0f;
+                    });
+            }
         }
 
         private void OnLoadSceneButtonClicked(Scene? scene)
