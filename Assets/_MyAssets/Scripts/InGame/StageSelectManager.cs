db6fd8157014e44c1b8cff2d3e921f0006e1d306
yuuki iwabuchi <yuki.iwabuchi.zeno1117@gmail.com>
2025-04-19 23:27:14 +0900

    ステージセレクトの変数の追加と変更
    ステージセレクトの動作に関する関数の追加をしました

--- Assets/_MyAssets/Scripts/InGame/StageSelectManager.cs
+++ Assets/_MyAssets/Scripts/InGame/StageSelectManager.cs
@@ -1,11 +1,35 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using NGeneral;
 
-public class StageSelectManager : MonoBehaviour
+namespace NInGame
 {
-    // ステージ1に移動
-    public void LoadStage1()
+    public sealed class StageSelectManager : MonoBehaviour
     {
-        SceneManager.LoadScene("Stage1");
+        [SerializeField] private Button button1;
+        [SerializeField] private Button button2;
+        [SerializeField] private Button button3;
+        [SerializeField] private Button button4;
+        [SerializeField] private Button button5;
+        [SerializeField] private Button button6;
+        [SerializeField] private Button button7;
+        [SerializeField] private Button button8;
+
+        private void Start()
+        {
+            ExecuteEvent(button1, Scene.Stage1);
+            ExecuteEvent(button2, Scene.Stage2);
+            ExecuteEvent(button3, Scene.Stage3);
+            ExecuteEvent(button4, Scene.Stage4);
+            ExecuteEvent(button5, Scene.Stage5);
+            ExecuteEvent(button6, Scene.Stage6);
+            ExecuteEvent(button7, Scene.Stage7);
+            ExecuteEvent(button8, Scene.Stage8);
+        }
+        private void ExecuteEvent(Button button,Scene scene)
+        {
+            if (button != null)
+                button.onClick.AddListener(() => scene.Load());
+        }
     }
 }
